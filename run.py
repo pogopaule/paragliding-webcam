fdbb81c9e09a547595c49f3630de1584b76fb4e5
pogopaule <pogopaule@gmx.de>
2016-03-30 11:16:56 +0200

    add kandel webcam

--- run.py
+++ run.py
@@ -1,6 +1,25 @@
 import os
+import datetime
+
 from imgurpython import ImgurClient
+from imgurpython.helpers.error import ImgurClientError
 
 def run():
     client = ImgurClient(os.environ['IMGUR_CLIENT_ID'], os.environ['IMGUR_CLIENT_SECRET'], os.environ['IMGUR_ACCESS_TOKEN'], os.environ['IMGUR_REFRESH_TOKEN'])
-    client.upload_from_url("http://www.elztalflieger1.de/webcamsp/images/current.jpg", config={'album': '1EN5T'}, anon=False)
+
+    try:
+        url = "http://www.elztalflieger1.de/webcamsp/images/current.jpg"
+        client.upload_from_url(url, config={'album': '1EN5T'}, anon=False)
+        print("Uploaded " + url)
+    except ImgurClientError as e:
+        print(e.error_message)
+
+    try:
+        time = datetime.datetime.now()
+        hour = str(time.hour + 2).zfill(2)
+        minute = str(time.minute / 15 * 3).zfill(2)
+        url = "http://www.dgfc-suedschwarzwald.de/webcam2/image-{0}-{1}.jpg".format(hour, minute)
+        client.upload_from_url(url, config={'album': 'BN5AE'}, anon=False)
+        print("Uploaded " + url)
+    except ImgurClientError as e:
+        print(e.error_message)
