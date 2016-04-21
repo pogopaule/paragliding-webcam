d520f4e434007ba4d5becab37a726c3e19e7e75e
pogopaule <pogopaule@gmx.de>
2016-04-22 00:12:17 +0200

    update download.py

--- download.py
+++ download.py
@@ -5,14 +5,40 @@ import time
 from imgurpython import ImgurClient
 from imgurpython.helpers.error import ImgurClientError
 
+places = [{
+    'name': 'Kandel',
+    'albums': [
+        {
+            'name': 'positive',
+            'id': '0XfM8',
+        }, {
+            'name': 'negative',
+            'id': 'NX3p8'
+        }
+    ]
+}, {
+    'name': 'Gschasi',
+    'albums': [
+        {
+            'name': 'positive',
+            'id': 'OthOo',
+        }, {
+            'name': 'negative',
+            'id': 'jQIwZ'
+        }
+    ]
+}]
+
 client = ImgurClient(os.environ['IMGUR_CLIENT_ID'], os.environ['IMGUR_CLIENT_SECRET'], os.environ['IMGUR_ACCESS_TOKEN'], os.environ['IMGUR_REFRESH_TOKEN'])
 
-for image in client.get_album_images('1EN5T'):
-    filename = time.strftime('%Y-%m-%d_%H-%M.jpg', time.localtime(image.datetime))
-    fullfilename = os.path.join( '/Users/fabian/Downloads/paragliding-webcam/gschasi/', filename)
-    urllib.urlretrieve(image.link, fullfilename)
+for place in places:
+    for album in place['albums']:
 
-for image in client.get_album_images('BN5AE'):
-    filename = time.strftime('%Y-%m-%d_%H-%M.jpg', time.localtime(image.datetime))
-    fullfilename = os.path.join( '/Users/fabian/Downloads/paragliding-webcam/kandel/', filename)
-    urllib.urlretrieve(image.link, fullfilename)
+        path = 'images/' + place['name'] + '/' + album['name']
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        for image in client.get_album_images(album['id']):
+            filename = time.strftime('%Y-%m-%d_%H-%M.jpg', time.localtime(image.datetime))
+            fullfilename = os.path.join( path, filename)
+            urllib.urlretrieve(image.link, fullfilename)
