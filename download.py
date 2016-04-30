2c9774a299bc2914c35ab00dd8a4f1b12009420a
pogopaule <pogopaule@gmx.de>
2016-04-30 17:26:32 +0200

    sample n images where n is the lowest album size

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import os
 import urllib
 import time
+import random
 
 from imgurpython import ImgurClient
 from imgurpython.helpers.error import ImgurClientError
@@ -29,16 +30,23 @@ places = [{
     ]
 }]
 
+
+def album_size(album):
+    return len(client.get_album_images(album['id']))
+
 client = ImgurClient(os.environ['IMGUR_CLIENT_ID'], os.environ['IMGUR_CLIENT_SECRET'], os.environ['IMGUR_ACCESS_TOKEN'], os.environ['IMGUR_REFRESH_TOKEN'])
 
 for place in places:
+
+    min_image_number = min(map(album_size, place['albums']))
+
     for album in place['albums']:
 
         path = 'images/' + place['name'] + '/' + album['name']
         if not os.path.exists(path):
             os.makedirs(path)
 
-        for image in client.get_album_images(album['id']):
+        for image in random.sample(client.get_album_images(album['id']), min_image_number):
             filename = time.strftime('%Y-%m-%d_%H-%M.jpg', time.localtime(image.datetime))
             fullfilename = os.path.join( path, filename)
             urllib.urlretrieve(image.link, fullfilename)
