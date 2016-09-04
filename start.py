18c51e0c5c3bb34d8fb2d1ef5c23b70c50f19584
pogopaule <pogopaule@gmx.de>
2016-09-04 15:36:51 +0200

    don't notify every 15 minutes
    only between 9 and 21 o'clock and wait 3 hours after a notification

--- start.py
+++ start.py
@@ -10,22 +10,31 @@ def calculate_probability(url):
     return kandelcaffe.digits.classify('kandelcaffe/model/kandel.caffemodel', 'kandelcaffe/model/deploy.prototxt', [url], 'kandelcaffe/model/mean.binaryproto', None, False)
 
 def job():
+    global delay_countdown
     print '------------------'
     print datetime.datetime.now()
 
     hour = datetime.datetime.now().hour
-    url = common.utils.current_image_url(0)
-    print url
-    try:
-        prob = calculate_probability(url)
-        print prob
-        if prob > 0.6:
-            campaign_id = mailchimp.sendmail.create_campaign(url)
-            mailchimp.sendmail.send_mail(campaign_id)
-            print 'mail!'
-    except IOError:
-        print 'Could not load image'
+    if hour > 8 and hour < 22 and delay_countdown <= 0:
+        url = common.utils.current_image_url(0)
+        print url
+        try:
+            prob = calculate_probability(url)
+            print prob
+            if prob > 0.6:
+                campaign_id = mailchimp.sendmail.create_campaign(url)
+                mailchimp.sendmail.send_mail(campaign_id)
+                delay_countdown = 12
+                print 'mail!'
+        except IOError:
+            print 'Could not load image'
+    else:
+        print 'sleeping'
 
+    delay_countdown = delay_countdown - 1
+
+
+delay_countdown = 0
 schedule.every(15).minutes.do(job)
 
 job()
